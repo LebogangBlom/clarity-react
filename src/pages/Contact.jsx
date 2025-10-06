bcebda09de42f6b471846722b19c8c98af49f41f
nopremacyteam <nopremacydivision@gmail.com>
2025-10-06 11:09:48 +0200

    fix: form action to /thank-you (match App route)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -143,7 +143,7 @@ export default function Contact(){
           method="POST"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
-          action="/ThankYou"
+          action="/thank-you"
           className="cly-contact-form"
           id="contactForm"
           noValidate
